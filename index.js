3c52f619abf77a2e7b3f6907284672acf3c5be65
shofi384@gmail.com Rahman <shofi384@gmail.com>
2018-07-10 15:25:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,5 +9,8 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
-  
+  var newObj = Object.assign({}, obj);
+  delete newObj.key;
+  return newObj;
 }
+
